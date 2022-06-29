8bad3ea2f868b1121b3f3040691fb80924157b3a
Thomas Rohloff <v10lator@myway.de>
2022-06-29 14:12:57 +0200

    First round of fixes.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include <sysapp/launch.h>
 #include <whb/log.h>
 #include <whb/log_udp.h>
+#include <whb/proc.h>
 
 #define MOV_R0_0            0xE3A00000
 #define BX_LR               0xE12FFF1E
@@ -19,28 +20,50 @@
 
 int main()
 {
+    WHBProcInit();
     WHBLogUdpInit();
-    WHBLogPrint("Opening iosuhax...");
-    if(IOSUHAX_Open(NULL) >= 0)
+    if(WHBProcIsRunning())
     {
-        WHBLogPrint("Patch 1...");
-        // move filename argument to new position in full path
-        IOSUHAX_kern_write32(0x050089B4, STR_R6_SP_0);
+        if(IOSUHAX_Open(NULL) >= 0)
+        {
+            // patch OS launch sig check
+            WHBLogPrint("Patch 1...");
+            IOSUHAX_kern_write32(0x0500A818, MOV_R0_0_MOV_R0_0);
 
-        WHBLogPrint("Patch 2...");
-        // change dynamically generated title path to wii vc title path
-        IOSUHAX_kern_write32(0x05008A54, 0x05062038); // /%s/code/%s
-        IOSUHAX_kern_write32(0x05008A58, 0x05074A18); // wii vc path
+            // move filename argument to new position in full path
+            WHBLogPrint("Patch 2...");
+            IOSUHAX_kern_write32(0x050089B4, STR_R6_SP_0);
 
-        WHBLogPrint("Closing iosuhax...");
-        IOSUHAX_Close();
-//        _SYSLaunchTitleWithStdArgsInNoSplash(FW_TID, NULL);
-        SYSLaunchMenu();
+            // change dynamically generated title path to wii vc title path
+            WHBLogPrint("Patch 3...");
+            IOSUHAX_kern_write32(0x05008A54, 0x05062038); // /%s/code/%s
+            IOSUHAX_kern_write32(0x05008A58, 0x05074A18); // wii vc path
+
+            // nop out memcmp hash checks
+            WHBLogPrint("Patch 4...");
+            IOSUHAX_kern_write32(0x040017E0, MOV_R0_0);
+            IOSUHAX_kern_write32(0x040019C4, MOV_R0_0);
+            IOSUHAX_kern_write32(0x04001BB0, MOV_R0_0);
+            IOSUHAX_kern_write32(0x04001D40, MOV_R0_0);
+
+            WHBLogPrint("Patch 5...");
+            IOSUHAX_kern_write32(0x01555500, 0);
+
+            WHBLogPrint("Closing iosuhax...");
+            IOSUHAX_Close();
+            _SYSLaunchTitleWithStdArgsInNoSplash(FW_TID, NULL);
+        }
+        else
+            WHBLogPrint("Error opening iosuhax...");
+
+        while(WHBProcIsRunning())
+            WHBLogPrint("Proc loop...");
     }
     else
-        SYSLaunchMenu();
+        WHBLogPrint("Proc error!");
 
     WHBLogPrint("Returning...");
     WHBLogUdpDeinit();
+//    WHBProcShutdown(); // TODO: Re-enable after HBL
     return 0;
 }
